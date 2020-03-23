dd15b3f673d0b06ed70cd00106edb18ed1bbe03b
Jam52 <jamie.sajdak@gmail.com>
2020-03-23 16:43:37 -0400

    add scroll-to-top link, and fuctionality

--- js/app.js
+++ js/app.js
@@ -17,8 +17,11 @@
  * Define Global Variables
  * 
 */
+const startingTime = performance.now();
 let navElements = document.querySelectorAll('[data-nav]');
 let navBar = document.querySelector('#navbar__list');
+let navMenu = document.querySelector('.navbar__menu');
+let scrollLink = document.querySelector('#scroll__link')
 
 
 /**
@@ -26,14 +29,6 @@ let navBar = document.querySelector('#navbar__list');
  * Start Helper Functions
  * 
 */
-function getNavData (elements) {
-    const navData = [];
-    for (element of elements) {
-        navData.push(element.getAttribute('data-nav'));
-    }
-    return navData;
-}
-
 function setSectionHighlight () {
     let navBarLinks = document.querySelectorAll('.menu__link');
     for(element of navElements) {
@@ -41,11 +36,10 @@ function setSectionHighlight () {
         const navIndex =  Array.prototype.indexOf.call(navElements, element);
         if (section.getBoundingClientRect().top > -100 && section.getBoundingClientRect().top < (window.innerHeight-200)) {
             section.classList.add('your-active-class');
-            console.log(navIndex);
-            console.log(navBarLinks.item(navIndex).classList.add('highlight_link'));
+            navBarLinks.item(navIndex).classList.add('highlight_link');
         } else {
             section.classList.remove('your-active-class');
-            console.log(navBarLinks.item(navIndex).classList.remove('highlight_link'));
+            navBarLinks.item(navIndex).classList.remove('highlight_link');
         }
     };
 
@@ -65,38 +59,30 @@ for (let i = 0; i < navElements.length; i++) {
     let link = document.createElement('a');
     let li = document.createElement('li');
     link.setAttribute('class', 'menu__link');
-    li.setAttribute('data-scroll', i)
-    li.textContent = navElements[i].querySelector('h2').textContent;
+    link.setAttribute('href','');
+    li.setAttribute('data-scroll', i);
+    link.setAttribute('data-scroll', i)
+    li.textContent = navElements[i].getAttribute('data-nav');
     link.appendChild(li);
     navFragment.appendChild(link);
 }
 navBar.appendChild(navFragment);
 
-/*let scrollY = window.scrollY
-window.addEventListener('scroll', function(){
-    if (window.scrollY === 0) {
-        console.log('at top');
-        navBar.setAttribute('class','visible');
-    } else {
-        console.log('not at top');
-        navBar.setAttribute('class','hidden');
-    }
-    scrollY = window.screenY;
-}) */
-
 // Add class 'active' to section when near top of viewport
-window.addEventListener('scroll', function (){
+window.addEventListener('scroll', () => {
     setSectionHighlight();
 });
 
 // Scroll to anchor ID using scrollTO event
-navBar.addEventListener('click', function (event) {
+navBar.addEventListener('click',(event) => {
+    event.preventDefault();
     const t = event.target;
     const top = document.querySelector('#'+navElements[t.getAttribute('data-scroll')].getAttribute('id'));
     top.scrollIntoView({behavior: "smooth"});
 });
 
-
+const endingTime = performance.now();
+console.log(startingTime - endingTime);
 /**
  * End Main Functions
  * Begin Events
@@ -109,4 +95,36 @@ navBar.addEventListener('click', function (event) {
 
 // Set sections as active
 
+
+//hide navbar when not scrolling
+let scrollY = window.scrollY;
+window.addEventListener('scroll', () => {
+    navMenu.classList.remove('hidden');
+    
+    setTimeout(()=>{
+        if(scrollY > window.innerHeight) {
+            scrollLink.classList.remove('hidden');
+        } else {
+           scrollLink.classList.add('hidden');
+        }
+        navMenu.classList.remove('hidden');
+        if(scrollY > 200) {
+            navMenu.classList.add('hidden');
+            };
+        scrollY = window.scrollY;
+    }, 1000);
+});
+
+
+//scroll to top listener
+
+scrollLink.addEventListener('click', (event) => { 
+    event.preventDefault();
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+      });
+    console.log('hiding scrollLink');
+});
 
